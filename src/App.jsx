016699b56ee27b68e271cc26cbc0a72d2d5dce62
ProdipKumarShil <prodipkrishna01@gmail.com>
2024-04-03 17:22:53 +0600

    added recent work card

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import Banner from "./components/Banner/Banner"
 import CaseStudies from "./components/CaseStudies/CaseStudies"
 import MenuBar from "./components/Menu/MenuBar"
 import Navbar from "./components/Navbar/Navbar"
+import RecentWork from "./components/RecentWork/RecentWork"
+
+import 'swiper/swiper-bundle.css';
 
 const App = () => {
   return (
@@ -9,6 +12,7 @@ const App = () => {
       <Navbar />
       <Banner />
       <CaseStudies />
+      <RecentWork />
       {/* <a class="mt-8 inline-flex items-center justify-center rounded-xl bg-secondary py-3 px-6 font-ibm text-base font-medium text-white shadow-xl shadow-[#3f8e0059] transition-transform duration-200 ease-in-out hover:scale-[1.02]"
         href="#">
         Sign up for free
